681a393c25ae72f2e2fcd05d00b00479989a4450
Allan Fernandes <Fernzallan14@gmail.com>
2022-08-20 00:34:55 +0100

    :octocat: search filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './index.css';
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters:[],
+      searchField: ''
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+componentDidMount()  {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(() => {
+      return {
+        monsters:users
+      }
+    }))
 }
 
-export default App;
+  render() {
+            const filterdMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+            });
+    return (
+      <div>
+          <input 
+            className='search-box' 
+            type='search' 
+            placeholder='enter monster name' 
+            onChange={(event) => {
+
+             const searchField = event.target.value.toLocaleLowerCase();
+
+             this.setState(() => {
+              return { searchField };
+             })
+            }}
+          />
+          {filterdMonsters.map((monster) => {
+            return <h1 key={monster.id}> {monster.name}</h1>
+          })}
+      </div>
+    )
+  }
+}
